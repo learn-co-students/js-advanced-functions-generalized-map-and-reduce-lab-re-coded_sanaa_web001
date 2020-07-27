696f5795c026b6641021b7bf5c9fa445bd1e7c39
Saleem Saleh <salem1234saleem@gmail.com>
2020-07-27 23:14:39 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-// Add your functions here
+function map(Array  , fun) {
+   return Array.map(x => fun(x));
+}
+
+function reduce(saleem ,func  ,startingPoint ) {
+  if (startingPoint > 0 ){
+    return startingPoint + saleem.reduce(func) ;
+  }else   {
+    return saleem.reduce(func);
+  }
+
+}
