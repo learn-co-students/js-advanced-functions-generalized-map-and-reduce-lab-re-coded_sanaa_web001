37fe19141989fb9d67001d97c4a23603d3702191
Nuha Al-Sameai <noha.pepeep1@gmail.com>
2020-08-14 07:27:55 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Add your functions here
+function map(sourceArray,fun){
+const newarr =sourceArray.map(fun);
+return newarr
+}
+
+
+function reduce(sourceArray,fun, startingPoint=0){
+  let newarr;
+  if(startingPoint!==0){
+   newarr= sourceArray.reduce(fun,startingPoint);}
+  else{
+   newarr= sourceArray.reduce(fun);
+  }
+  return newarr;
+}
