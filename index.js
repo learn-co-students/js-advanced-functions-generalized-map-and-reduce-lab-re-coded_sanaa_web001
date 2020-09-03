b9eacaa1f93584da19e8d0c07f3662faeb45e27c
Nusiba Ibrahim <ainusiba@gmail.com>
2020-09-03 07:40:28 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Add your functions here
+
+function map(arr, func){
+    let newArr = []
+    arr.forEach(element => {
+        newArr.push(func(element))
+    });
+    return newArr
+}
+
+function reduce(src, fnct, start){
+    let arr = (!!start) ? start : src[0];
+    let i = (!!start) ? 0 : 1 
+
+    for (; i < src.length; i++){
+        arr = fnct(src[i], arr)
+    }
+    return arr;
+}
