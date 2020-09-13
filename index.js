51c83dd74d545f6f34f515c6ae3586a862e0d56c
Tahany Alswoidy <tahanyalswoidy@gmail.com>
2020-09-13 06:45:12 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Add your functions here
+function map(sourceArray,myfunction){
+  return sourceArray.map(a=>myfunction(a));
+}
+function reduce(sourceArray,myfunction,starting){
+  if(starting){return sourceArray.reduce(myfunction,starting)}
+  else if(!starting){return sourceArray.reduce(myfunction)}
+}
