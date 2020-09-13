6fb87cec902c5608f980004ae226e5f20f144601
Marwa Alaghbri <marwaalaghbry@gmail.com>
2020-09-13 15:58:07 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Add your functions here
+function map(arr,fun){
+    const newArr = []
+    for (let i=0; i < arr.length; i++) {
+        newArr.push(fun(arr[i]))
+    }
+    return newArr
+}
+
+function reduce(arr, func, start){
+    let a= (!!start) ? start : arr[0];
+    let i = (!!start) ? 0 : 1 
+
+    for (; i < arr.length; i++){
+        a = func(arr[i], a)
+    }
+    return a;
+} 
