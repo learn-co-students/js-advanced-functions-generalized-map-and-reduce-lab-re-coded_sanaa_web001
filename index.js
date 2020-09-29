311866f672da7bab1f0ace49f919a9dcbbba2338
Hajar Helal <hajerhelal44@gmail.com>
2020-09-29 08:40:28 +0000

    Done.

--- index.js
+++ index.js
@@ -1,7 +1,16 @@
-function map(src) {
+function map(src,fun) {
   let r = []
   for (let i = 0; i < src.length; i++) {
-    r.push(-1 * src[i])
+    r.push( fun(src[i]))
+  }
+  return r;
+}
+function reduce(src,fun,op){
+  let r = (op) ? op : src[0]
+  let i = (op) ? 0 : 1
+
+for (;i < src.length; i++) {
+    r = fun(src[i], r)
   }
   return r;
 }
